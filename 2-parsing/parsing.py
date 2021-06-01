09d0a3c952ba31e999d3ce27c567ab70908b65bb
marzeckm <marzecki97@gmail.com>
2021-06-01 09:01:55 +0200

    regex task

--- 2-parsing/parsing.py
+++ 2-parsing/parsing.py
@@ -32,38 +32,48 @@ for passport in test:
 # ^[A-Z][a-z]+ [A-Z][a-z]+$
 
 def byr(text):
-    return True if re.search("^\d{4}$", text) and 2002 >= int(text) >= 1920 else False
+    return True if re.search(r"^\d{4}$", text) and 2002 >= int(text) >= 1920 else False
 
 
 def iyr(text):
-    return True if re.search("^\d{4}$", text) and 2020 >= int(text) >= 2010 else False
+    return True if re.search(r"^\d{4}$", text) and 2020 >= int(text) >= 2010 else False
 
 
 def eyr(text):
-    return True if re.search("^\d{4}$", text) and 2030 >= int(text) >= 2020 else False
+    return True if re.search(r"^\d{4}$", text) and 2030 >= int(text) >= 2020 else False
 
 
 def hgt(text):
-    if text[-2:] =='cm':
+    if text[-2:] == 'cm':
         text = text[:-2]
-        return True if re.search("^\d$", text) and 193 >= int(text) >= 150 else False
-    if text[-2:] =='in':
+        return True if re.search(r"^\d+$", text) and 193 >= int(text) and  int(text) >= 150 else False
+    if text[-2:] == 'in':
         text = text[:-2]
-        return True if re.search("^\d$", text) and 76 >= int(text) >= 59 else False
+        return True if re.search(r"^\d+$", text) and 76 >= int(text) >= 59 else False
+
+
+def hcl(text):
+    return True if re.search(r"^#[0-9a-f]{6}$", text) else False
+
+
+def ecl(text):
+    return True if text in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'] else False
+
+
+def pid(text):
+    return True if re.search(r"^\d{9}$", text) else False
+
 
 not_passed2 = []
 
 for passport in test:
     if passport not in not_passed:
-        # for key in passport.keys():
-        for key in ['byr', 'iyr', 'eyr', 'hgt']:
-            if locals()[key](passport[key]):
-                print(passport)
+        for key in ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']:
+            if not locals()[key](passport[key]):
                 not_passed2.append(passport)
                 break
 
 print(len(test))
 print(len(test) - len(not_passed))
-print(len(not_passed2))
-
+print(len(test) - len(not_passed) - len(not_passed2))
 
